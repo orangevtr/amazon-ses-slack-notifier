744f4f4b14e7159266063c7e88aebb4638d89fd3
Takayuki Hirayama <orangevtr@server.fake>
2019-05-01 16:33:09 +0900

    cloned from serverless example

--- handler.js
+++ handler.js
@@ -1,14 +1,37 @@
 'use strict';
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
-  };
+const AWS = require('aws-sdk');
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+const s3 = new AWS.S3({
+    apiVersion: '2006-03-01',
+    region: process.env.AWSREGION,
+});
+
+const simpleParser = require('mailparser').simpleParser;
+
+module.exports.postprocess = async(event) => {
+    console.log('Received event:', JSON.stringify(event, null, 2));
+    const record = event.Records[0];
+    // Retrieve the email from your bucket
+    const request = {
+        Bucket: record.s3.bucket.name,
+        Key: record.s3.object.key,
+    };
+
+    try {
+        const data = await s3.getObject(request).promise();
+        console.log('Raw email:' + data.Body);
+        const email = await simpleParser(data.Body);
+        console.log('date:', email.date);
+        console.log('subject:', email.subject);
+        console.log('body:', email.text);
+        console.log('from:', email.from.text);
+        console.log('attachments:', email.attachments);
+        return {
+            status: 'success'
+        };
+    } catch (Error) {
+        console.log(Error, Error.stack);
+        return Error;
+    }
 };
