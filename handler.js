d0627d791c892a7ee84d34e53a992970ecafe518
Takayuki Hirayama <orangevtr@server.fake>
2019-05-04 18:09:42 +0900

    add simple code to notify with lib

--- handler.js
+++ handler.js
@@ -1,14 +1,58 @@
 'use strict';
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
-  };
+const AWS = require('aws-sdk');
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+const s3 = new AWS.S3({
+    apiVersion: '2006-03-01',
+    region: process.env.AWSREGION,
+});
+
+const simpleParser = require('mailparser').simpleParser;
+
+const notifier = require('./lib/slack-notify-message.js');
+
+const options = {
+    channel: '#sandbox',
+    icon_emoji: ':akiyoshi:',
+    username: 'notifierテストユーザ'
+};
+
+module.exports.postprocess = async(event) => {
+    console.log('Received event:', JSON.stringify(event, null, 2));
+    const record = event.Records[0];
+    // Retrieve the email from your bucket
+    const request = {
+        Bucket: record.s3.bucket.name,
+        Key: record.s3.object.key,
+    };
+
+    try {
+        const data = await s3.getObject(request).promise();
+        console.log('Raw email:' + data.Body);
+        const email = await simpleParser(data.Body);
+        console.log('date:', email.date);
+        console.log('subject:', email.subject);
+        console.log('body:', email.text);
+        console.log('from:', email.from.text);
+        console.log('attachments:', email.attachments);
+
+        const message = {
+            author_name: email.from.text,
+            title: email.subject,
+            body: email.text,
+            footer: 'amazon-ses-slack-notifier',
+            date: email.date
+        };
+
+        console.log(JSON.stringify(message))
+
+        notifier(message, null, options);
+
+        return {
+            status: 'success'
+        };
+    } catch (Error) {
+        console.log(Error, Error.stack);
+        return Error;
+    }
 };
